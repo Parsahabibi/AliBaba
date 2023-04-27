00d4b23f99380062ec6938f4de9110f19c627873
Parsa <97088319+Parsahabibi@users.noreply.github.com>
2023-04-27 16:55:45 +0430

    Start AliBaba

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
