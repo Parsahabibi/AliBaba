f5b05d79ba42153973fe6b618b460e65876d6af1
Parsa <97088319+Parsahabibi@users.noreply.github.com>
2023-04-29 15:55:21 +0430

    Mobile Header fixed

--- src/components/Header/MobileHeader/MobileHeader.jsx
+++ src/components/Header/MobileHeader/MobileHeader.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+
 import { Airplane, Bus, Camp, Hotel, Tour, Train } from '../../icons/Icons'
+import { Link } from 'react-router-dom'
 const MobileHeader = () => {
+
+
+    const icon = [{id:1 ,title: <Airplane />}, {id:2 ,title: <Train />}, {id:3 ,title: <Bus />}, {id:4 ,title: <Hotel />}, {id:5 ,title: <Tour />}, {id:6 ,title: <Camp />}]
+
     return (
         <div>
             <div className='bgColorHeader'>
@@ -9,13 +15,14 @@ const MobileHeader = () => {
                     <img src={require('../../../assets/image/Footer/AlibabaIcon.png')} />
                 </div>
                 <div className='iconMobileHeader'>
-                    {/* icon */}
-                    <Airplane />
-                    <Train />
-                    <Bus />
-                    <Hotel />
-                    <Tour />
-                    <Camp />
+                {
+                    icon.map(
+                        item=>
+                        <Link to={""} key={item.id}>
+                           {item.title}
+                        </Link>
+                    )
+                }
                 </div>
             </div>
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import router from './router/router'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./Style/scss/style.scss"
+import {
+  RouterProvider,
+} from "react-router-dom";
+import router from './router/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
