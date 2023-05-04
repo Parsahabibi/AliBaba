b2453e6ceb633b9cfe6d2faaeb4ae8200b1f0ae2
Amirreza <amirrezakashipazha@gmail.com>
2023-05-04 11:21:52 +0430

    your questions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,29 +2,21 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import OtherService from '../components/OtherService/OtherService';
-<<<<<<< HEAD
 import Questions from '../components/Yourquestion/YourQuestion';
-=======
-import HomePageContent from '../components/HomePageContent/HomePageContent'
+import HomePageContent from '../components/HomePageContent/HomePageContent';
 
->>>>>>> 480d23560d341197d660f1cf718abe2ba0190191
 const Home = () => {
   return (
     <div>
       <Header />
       <div style={{ paddingTop: "120px", width: "90%", margin: "0 auto" }}>
         <OtherService />
+        <Questions />
         <HomePageContent />
-        <Footer />
       </div>
-<<<<<<< HEAD
-      <Questions />
       <Footer />
-
-=======
->>>>>>> 480d23560d341197d660f1cf718abe2ba0190191
     </div>
-  )
+  );
 }
 
 export default Home;
